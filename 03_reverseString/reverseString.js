154b0a13b458c15aeec10e68a00941d8473426c7
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-10-20 17:41:58 -0400

    Include solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (i = 0; i < string.length; i++) {
+      reversedString = string.charAt(i) + reversedString;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
