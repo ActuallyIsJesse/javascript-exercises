7691c3a39380893c364e0a99f09fe76c5d330c56
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-10-20 15:10:42 -0400

    Solve

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatinatedString = string;
+    for (let i = 1; i < num; i++) {
+        concatinatedString = concatinatedString + string;
+    }
+    return concatinatedString;
 };
 
 // Do not edit below this line
