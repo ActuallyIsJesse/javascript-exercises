5bec227994ff60c113d93823d8a607b4e2538710
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-10-21 17:44:50 -0400

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2) || num1 === NaN || num2 === NaN) {
+        return "ERROR"
+    }
+    const rangeLow = num1 <= num2 ? num1 : num2;
+    const rangeHigh = num1 >= num2 ? num1 : num2;
+    let finalSum = rangeLow - 1;
+    for (let i = rangeLow; i <= rangeHigh; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
