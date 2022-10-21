ef50dba24cc85498da0ba55da4b1e80717483987
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-10-21 15:10:08 -0400

    Add solution to pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...args) {
+    const newArray = arguments[0];
+    for (i of newArray) {
+        if (newArray[i] === arguments[1]) {
+            newArray.splice(i, 1);
+        }
+    }
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
