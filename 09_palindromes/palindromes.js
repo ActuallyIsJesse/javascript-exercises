51a2abb3f06a22755983189dc251a88a4e1606bb
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-11-16 07:14:16 -0500

    Include solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+let str = input.toLowerCase();
+str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+str = str.replace(/\s/g, '');
+let reversedString = "";
+for (i = 0; i < str.length; i++) {
+    reversedString = str.charAt(i) + reversedString;
+  }
+ console.log({str});
+ console.log({reversedString});
+return reversedString === str;
 };
 
 // Do not edit below this line
