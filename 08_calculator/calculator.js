b9fba8f3ad173240b549f45b4dac3604606d0696
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-11-16 06:46:42 -0500

    Include solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+  let sumTotal = 0;
+  inputArray.forEach(element => {
+    sumTotal += element;
+  });
+  return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  let multipliedTotal = 1;
+  inputArray.forEach(element => {
+    multipliedTotal *= element;
+  });
+  return multipliedTotal;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0) {return 1};
+  let factorialNumber = number;
+  for(let i = number - 1; i >= 1; i--) {
+    factorialNumber *= i ;
+  }
+	return factorialNumber;
 };
 
 // Do not edit below this line
