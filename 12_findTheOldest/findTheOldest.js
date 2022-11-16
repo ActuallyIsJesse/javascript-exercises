3d94af63bc3c6006d18732ca53b3e3d60e1686a3
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-11-16 14:33:50 -0500

    Include solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(input) {
+    const oldest = input.sort((a,b) => {
+        const inputA = !a.yearOfDeath ? (new Date().getFullYear() - a.yearOfBirth) : a.yearOfDeath - a.yearOfBirth;
+        const inputB = !b.yearOfDeath ? (new Date().getFullYear() - b.yearOfBirth) : b.yearOfDeath - b.yearOfBirth;
+        return inputA > inputB ? -1 : 1;
+    });
+    return oldest[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
